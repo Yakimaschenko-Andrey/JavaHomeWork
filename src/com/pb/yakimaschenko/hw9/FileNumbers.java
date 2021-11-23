045438ac566239fc06a7099e0c5e24b94ce8f9a8
Yakimaschenko Andrey <andrej.jakimascenko@privatbank.ua>
2021-11-24 00:09:16 +0200

    modify hw9

--- src/com/pb/yakimaschenko/hw9/FileNumbers.java
+++ src/com/pb/yakimaschenko/hw9/FileNumbers.java
@@ -1,12 +1,14 @@
 package com.pb.yakimaschenko.hw9;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class FileNumbers {
 
     public static void main(String[] args) throws Exception {
 
        createNumbersFile();
+       createOddNumbersFile();
 
 
     }
@@ -31,10 +33,28 @@ public class FileNumbers {
     }
 
     public static void createOddNumbersFile() throws Exception{
-        try(Reader reader = new FileReader("D:\\Users\\dn260392jan1\\Desktop\\JAVA instal\\CURS\\HOME WORK\\numbers.txt")) {
+        try (Scanner in = new Scanner(new File("D:\\Users\\dn260392jan1\\Desktop\\JAVA instal\\CURS\\HOME WORK\\numbers.txt"))) {
+            PrintWriter printWriter = new PrintWriter(new File("D:\\Users\\dn260392jan1\\Desktop\\JAVA instal\\CURS\\HOME WORK\\odd-numbers.txt"));
+            System.out.println("Заменить все четные числа на 0:");
+            while (in.hasNextLine()) {
+                Scanner scan = new Scanner(in.nextLine());
+                while (scan.hasNextInt()) {
+                    int change = scan.nextInt();
+                    if (change % 2 == 0) {
+                        System.out.println("0");
+                        change = 0;
+                    } else
+                        System.out.println(change + " ");
+                    printWriter.println(change + " ");
+                }
+                scan.close();
+               // printWriter.println();
 
-        }catch (Exception e) {
-            System.out.println("Ошибка при чтении файла: "+ e);
+            }
+            System.out.println();
+            printWriter.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи файла: " + e);
         }
 
     }
